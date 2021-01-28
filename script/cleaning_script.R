ec8c8c139194162c7f7a0f0fbd4ec3aa9ad710b2
amanda-mari <amari704@gmail.com>
2021-01-27 22:12:24 -0500

    Merged all sheets into 1 and removed the redundant id variable

--- script/cleaning_script.R
+++ script/cleaning_script.R
@@ -2,9 +2,10 @@
 library("tidyverse")
 library("here")
 library("readxl")
+library("data.table")
 
 # Functions ---------------------------
-source("functions_script.R")
+source("script/functions_script.R")
 
 # Load data ---------------------------
 all_sheets <- readxl::excel_sheets(path = here::here("data/raw/raw_data.xlsx"))
@@ -13,7 +14,10 @@ all_sheets <- readxl::excel_sheets(path = here::here("data/raw/raw_data.xlsx"))
 # I only need the following sheets: "All," "ELL," "SWD," "Ethnicity," "Gender,"
 # "Poverty," and "Ever ELL".
 
-needed_sheets <- all_sheets[all_sheets %in% c("All", "ELL", "SWD", "Ethnicity", "Gender", "Poverty", "Ever ELL")]
+needed_sheets <- all_sheets[all_sheets %in% c(
+  "All", "ELL", "SWD", "Ethnicity",
+  "Gender", "Poverty", "Ever ELL"
+)]
 
 hs_data <- lapply(
   needed_sheets,
@@ -23,10 +27,27 @@ hs_data <- lapply(
 
 # Rename Columns ---------------------------
 
-  # Each sheet has the same column names in the same order
+# Each sheet has the same column names in the same order
+old_column_names <- c(
+  "...1","DBN","School Name", "Category",
+  "Cohort Year", "Cohort", "# Total Cohort",
+  "# Grads", "% Grads", "# Total Regents",
+  "% Total Regents of Cohort",
+  "% Total Regents of Grads",
+  "# Advanced Regents",
+  "% Advanced Regents of Cohort",
+  "% Advanced Regents of Grads",
+  "# Regents without Advanced",
+  "% Regents without Advanced of Cohort",
+  "% Regents without Advanced of Grads",
+  "# Local", "% Local of Cohort",
+  "% Local of Grads", "# Still Enrolled",
+  "% Still Enrolled", "# Dropout",
+  "% Dropout"
+)
 
 new_column_names <- c(
-  "id", "dbn", "school_name", "demo_group",
+  "id", "dbn","school_name", "demo_group",
   "cohort_start", "cohort_type", "demo_group_size",
   "demo_grad_total", "demo_grad_rate",
   "demo_regents_total", "demo_regents_grad_rate",
@@ -41,4 +62,21 @@ new_column_names <- c(
   "demo_dropout_total", "percent_cohort_dropout"
 )
 
-hs_data <-lapply(hs_data, setNames, new_column_names)
+hs_data <- lapply(hs_data, setnames, old_column_names, new_column_names)
+
+# Data Merging ---------------------------
+
+# Merge all sheets into one
+
+combined_data <- hs_data %>% reduce(full_join)
+
+#The "id" column is a string of characters pasted together from the other columns, we do not need it
+
+combined_data <-subset(combined_data,select=-id)
+
+# Data Wrangling ---------------------------
+# The data is currently in wide format with several columns.
+# It will be easier for analysis to have it in wide format.
+
+
+
